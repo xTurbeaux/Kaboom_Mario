d4a0166582fe2be5a1b6ab13590e1b1ae574f284
Aaron Mayhew <standingbytocopy@gmail.com>
2021-06-26 15:13:31 -0500

    base game layer

---
+++ game.js
@@ -0,0 +1,51 @@
+kaboom({
+  global: true,
+  fullscreen: true,
+  scale: 1,
+  debug: true,
+  clearColor: [0, 0, , 1]
+})
+
+loadRoot('https://i.imgur.com/');
+loadSprite('coin', 'wbKxhcd.png');
+loadSprite('evil-shroom', 'KPO3fR9.png');
+loadSprite('brick', 'pogC9x5.png');
+loadSprite('block', 'bdrLpi6.png');
+loadPrite('mario', 'Wb1qfhK.png');
+loadSprite('mushroom', '0wMd92p.png');
+loadSprite('surprise', 'gesQ1KP.png');
+loadSprite('unboxed', 'bdrLpi6.png');
+loadSprite('pipe-top-left', 'ReTPiWY.png');
+loadSprite('pipe-top-right', 'hj2GK4n.png');
+loadSprite('pipe-top-top', 'c1cYSbt.png');
+loadSprite('pipe-top-bottom', 'nqQ79eI.png');
+
+
+screen("game", () => {
+  layers(['bg', 'obj', 'ui'], 'obj')
+
+  const map = [
+    '                                              ',
+    '                                              ',
+    '                                              ',
+    '                                              ',
+    '                                              ',
+    '                                              ',
+    '                                              ',
+    '                                              ',
+    '                                              ',
+    '=============================  ==============='
+  ]
+
+  const levelCfg = {
+    width: 20,
+    height: 20,
+    '=':[sprite('block')],
+
+  };
+
+
+});
+
+
+start ("game")
